2983e97ee38171509066e47f5e79f7cbfc87085f
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-07-25 13:36:50 +0800

    Leroux Y Xchire

--- app/ModuleSales/components/Task/ScheduledActivity/TableXchire.tsx
+++ app/ModuleSales/components/Task/ScheduledActivity/TableXchire.tsx
@@ -67,9 +67,6 @@ const formatDate = (timestamp: string) => {
       year: "numeric",
       month: "long",
       day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
       timeZone: "Asia/Manila",
     }).format(new Date(timestamp));
   };
