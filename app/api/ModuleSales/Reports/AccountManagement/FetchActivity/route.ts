c9ff49c2c4c0eec936dbd098674064212c0b811b
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-10-06 15:36:14 +0800

    Leroux Y Xchire

--- app/api/ModuleSales/Reports/AccountManagement/FetchActivity/route.ts
+++ app/api/ModuleSales/Reports/AccountManagement/FetchActivity/route.ts
@@ -14,7 +14,7 @@ const Xchire_sql = neon(Xchire_databaseUrl);
  */
 export async function GET() {
     try {
-        const Xchire_fetch = await Xchire_sql`SELECT * FROM activity ORDER BY date_updated;`;
+        const Xchire_fetch = await Xchire_sql`SELECT * FROM activity;`;
 
         console.log("Xchire fetched accounts:", Xchire_fetch);
 
