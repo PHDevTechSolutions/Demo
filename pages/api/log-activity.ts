19ec0d9c2510100b8f613f04d524cb40f07e3da2
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-10-21 07:02:32 +0800

    Update log-activity.ts

--- pages/api/log-activity.ts
+++ pages/api/log-activity.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'; 
 import { connectToDatabase } from '@/lib/MongoDB';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
