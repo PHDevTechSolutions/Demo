76c328e845a91afd99daeaf283642d6b6e07eee9
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-11-08 12:17:00 +0800

    Leroux Y Xchire

--- pages/api/gmail/auth.ts
+++ pages/api/gmail/auth.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { google } from "googleapis";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const oAuth2Client = new google.auth.OAuth2(
     process.env.GMAIL_CLIENT_ID,
     process.env.GMAIL_CLIENT_SECRET,
@@ -10,11 +10,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const SCOPES = ["https://www.googleapis.com/auth/gmail.send"];
 
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline", // para maibalik ang refresh token
-    prompt: "consent",      // laging ipapakita consent screen para makuha refresh token
+  const url = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent", // para siguradong may refresh token
     scope: SCOPES,
   });
 
-  res.status(200).json({ authUrl });
+  res.redirect(url);
 }
